bfe80cc539afc10d118e98b0d420cab0de659232
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2025-04-23 23:40:20 +0100

    small changes

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <GithubProvider>
+      <App />
+    </GithubProvider>
   </React.StrictMode>
 );
 

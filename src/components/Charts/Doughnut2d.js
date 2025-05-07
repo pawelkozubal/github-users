23b204a48dc8c215723fd1c6cc66c48303d15b48
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2025-05-07 22:43:58 +0100

    added charts and finish language pie chart

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.ocean';
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+
+
+const ChartComponent = ({data}) => { 
+
+  const chartConfigs = {
+    type: 'doughnut2d', 
+    width: '100%', 
+    height: '400', 
+    dataFormat: 'json',
+    dataSource: {
+      Chart: { 
+        caption: 'Stars Per Language',
+        decimals: 0, 
+        doughnutRadius: '45%',
+        showPercentValues: 0,
+        theme: 'ocean',
+      },
+      data,
+    }
+  }
+  return <ReactFC {...chartConfigs}/>;
 };
 
-export default Doughnut2d;
+export default ChartComponent;
--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.ocean';
 
-const Pie3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+
+
+const ChartComponent = ({data}) => { 
+
+  const chartConfigs = {
+    type: 'pie2d', 
+    width: '100%', 
+    height: '400', 
+    dataFormat: 'json',
+    dataSource: {
+      Chart: { 
+        caption: ' Languages',
+        theme: 'ocean', 
+        decimals: 0, 
+        pieRadius: '35%',
+        
+      },
+      data,
+    }
+  }
+  return <ReactFC {...chartConfigs}/>;
 };
 
-export default Pie3D;
+export default ChartComponent;
